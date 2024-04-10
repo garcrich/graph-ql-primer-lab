a603ad07a622c7ac82c5d66ad7cdd895b72e3c6f
Ricky Garcia <rg3646@outlook.com>
2024-04-10 10:35:33 -0600

    intial setup

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GraphqlModule } from './graphql/graphql.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [GraphqlModule, PostsModule],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
